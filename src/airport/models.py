1620b6c15d38ec2b257bf00b9db986c07d429b02
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-02-21 14:31:20 +0200

    Create Airport, City, Country models

--- src/airport/models.py
+++ src/airport/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="cities")
+
+    def __str__(self):
+        return f"{self.name}({self.country.name})"
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name="airports")
+
+    def __str__(self):
+        return self.name
