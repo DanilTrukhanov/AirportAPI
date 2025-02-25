9838e8d2e30067c8446a546de5c4e5c4dfef16ea
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-02-25 17:07:08 +0200

    Change swagger path

--- src/airport_core/urls.py
+++ src/airport_core/urls.py
@@ -26,13 +26,8 @@ urlpatterns = [
     path("api/airport/", include("airport.urls", namespace="airport")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/swagger/",
+        "api/doc/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
-    ),
-    path(
-        "api/schema/redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc",
     ),
 ] + debug_toolbar_urls()
