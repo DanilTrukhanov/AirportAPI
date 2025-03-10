eb7e58a6489e9d0d13294e7d9a69b3e6e0c04dd3
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-03-10 13:53:32 +0200

    Remove return false statement

--- src/airport/permissions.py
+++ src/airport/permissions.py
@@ -5,6 +5,4 @@ class ReadOnlyUserOrIsAdminPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if request.user.is_authenticated and request.user.is_staff:
-            return True
-        return False
+        return request.user.is_authenticated and request.user.is_staff
